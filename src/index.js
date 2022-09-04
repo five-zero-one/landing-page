c87eb8590d85c6d6cea7e82e103b1c423c537c98
adoublef <kristopherab@gmail.com>
2022-09-04 20:38:07 +0100

    auto close menu

--- src/index.js
+++ src/index.js
@@ -14,7 +14,15 @@ const links = [...sections]
         anchor.textContent = textContent[0].toUpperCase() + textContent.substring(1);
         anchor.href = "#" + section.id;
 
+        anchor.addEventListener("click", jumpToSection);
+
         return item;
     });
 
 nav.append(...links);
+
+function jumpToSection(e) {
+    // e.preventDefault();
+
+    e.target.closest("header").querySelector("input").checked = false;
+}
